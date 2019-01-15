2a62d2c3682b46a4ed7b70f315cee9b415440674
Corrado Campisano <corrado.campisano@gmail.com>
2019-01-15 12:30:35 +0100

    cleaned up, with gradient colored unit circle to start with

---
+++ js/temp.js
@@ -0,0 +1,12 @@
+function createSquareArray(n) {
+  var arr = [];
+  for (var i=0;i<n;i++) {
+     arr[i] = [];
+  }
+  return arr;
+}
+
+var step = 36
+var originalPoints = createSquareArray(step);
+
+
